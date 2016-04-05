1d61ae4ba755cba069e07a5c36c81f6befdaeb00
shahoian <ruben.shahoyan@cern.ch>
2016-04-05 15:53:43 +0200

    added processing macro

---
+++ vClass/proj.C
@@ -0,0 +1,12 @@
+void proj(int run=245231
+	  ,Long64_t tmin=0
+	  ,Long64_t tmax= 9999999999
+	  ,const char * resList="lst.txt"	 
+	  )
+{
+  gROOT->ProcessLine(".L /home/shahoian/tpcCalib/vClass/AliTPCDcalibRes.cxx+g");
+  AliTPCDcalibRes* clb = new AliTPCDcalibRes(run, tmin, tmax, resList);
+  clb->SetOCDBPath("local:///cvmfs/alice.cern.ch/calibration/data/2015/OCDB");
+  clb->ProcessFromDeltaTrees();
+  clb->Save();
+}
